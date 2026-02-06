37a4c8bd6393f193347e95f7ada717400ebf7f46
Briyit Melany <briyitmelanycapcha@gmail.com>
2026-02-06 08:17:07 -0500

    Realizacion de Entidades de dominio e infraestructura junto con consumo de algunas api de Producto

--- src/main/java/org/equipo1/transacciones/infrastructure/adapter/output/persistence/jpa/CompraJpaRepository.java
+++ src/main/java/org/equipo1/transacciones/infrastructure/adapter/output/persistence/jpa/CompraJpaRepository.java
@@ -1,4 +1,14 @@
 package org.equipo1.transacciones.infrastructure.adapter.output.persistence.jpa;
 
-public interface CompraJpaRepository {
+import org.equipo1.transacciones.infrastructure.adapter.output.persistence.entity.CompraEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CompraJpaRepository extends JpaRepository<CompraEntity, Integer> {
+
+    List<CompraEntity> findByFechaCompraBetween(LocalDate inicio, LocalDate fin);
+
+    List<CompraEntity> findByEstado(String estado);
 }
